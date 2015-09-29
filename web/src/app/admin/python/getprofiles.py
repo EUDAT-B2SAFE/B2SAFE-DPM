32c850c16a4d92dda031c41b9cf52d75bc21a83c
Adil Hasan <adilhasan2@gmail.com>
2015-09-29 15:05:24 +0100

    bug fix getting the user from the environment. adil

--- web/src/app/admin/python/getprofiles.py
+++ web/src/app/admin/python/getprofiles.py
@@ -125,7 +125,7 @@ def getProfiles(config):
         if (config.has_option("HTMLENV", "user")):
             username = config.get("HTMLENV", "user").strip()
     elif (config.get("AUTHENTICATION", "type") == "AAI"):
-        username = os.environ[""]
+        username = os.environ["REMOTE_USER"].strip()
     else:
         username = os.environ["REMOTE_USER"].strip()
     
